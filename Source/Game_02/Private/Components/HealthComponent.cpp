d10a8c68a62c7ea60544c363c6b5380493d1ac2a
Leszek Górniak <leszekgorniak7@gmail.com>
2021-09-04 21:36:10 +0200

    Movement & Shooting

---
+++ Source/Game_02/Private/Components/HealthComponent.cpp
@@ -0,0 +1,67 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Components/HealthComponent.h"
+#include "GameFramework/Character.h"
+
+UHealthComponent::UHealthComponent()
+{
+	PrimaryComponentTick.bCanEverTick = false;
+}
+
+float UHealthComponent::GetCurrentHealth() const
+{
+	return CurrentHealth;
+}
+
+float UHealthComponent::GetHealthPercentage() const
+{
+	return CurrentHealth / MaxHealth;
+}
+
+void UHealthComponent::MakeDead()
+{
+	CurrentHealth = 0;
+	bIsAlive = false;
+
+	if (DeathAnimation && CharacterOwner)
+	{
+		CharacterOwner->PlayAnimMontage(DeathAnimation, 1.f, FName(""));
+	}
+
+	OnDeath.Broadcast();
+}
+
+float UHealthComponent::HandleDamage(AActor* DamagedActor, float Damage, const class UDamageType* DamageType, class AController* InstigatedBy, AActor* DamageCauser)
+{
+	if (bIsAlive)
+	{
+		CurrentHealth -= Damage;
+
+		if (CurrentHealth <= 0)
+		{
+			MakeDead();
+		}
+	}
+	return CurrentHealth;
+}
+
+void UHealthComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	CurrentHealth = MaxHealth;
+	CharacterOwner = Cast<ACharacter>(GetOwner());
+
+	DamageTaken.BindUFunction(this, "HandleDamage");
+	if (CharacterOwner)
+	{
+		CharacterOwner->OnTakeAnyDamage.Add(DamageTaken);
+	}
+	else
+	{
+		GetOwner()->OnTakeAnyDamage.Add(DamageTaken);
+	}
+	
+}
+
---
+++ Source/Game_02/Public/Components/HealthComponent.h
@@ -0,0 +1,54 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "HealthComponent.generated.h"
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnDeath);
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class GAME_02_API UHealthComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	UHealthComponent();
+
+	UPROPERTY(BlueprintAssignable, Category = "Health")
+	FOnDeath OnDeath;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Health")
+	float MaxHealth = 100;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Health")
+	bool bIsAlive = true;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Animations")
+	UAnimMontage* DeathAnimation = nullptr;
+
+	UFUNCTION(BlueprintPure, Category = "Health")
+	float GetCurrentHealth() const;
+
+	UFUNCTION(BlueprintPure, Category = "Health")
+	float GetHealthPercentage() const;
+
+	UFUNCTION()
+	float HandleDamage(AActor* DamagedActor, float Damage, const class UDamageType* DamageType, class AController* InstigatedBy, AActor* DamageCauser);
+
+protected:
+	virtual void BeginPlay() override;
+
+private:
+	ACharacter* CharacterOwner = nullptr;
+	
+	float CurrentHealth;
+
+	void MakeDead();
+
+	UAnimInstance* CharacterAnimInstance = nullptr;
+
+	TScriptDelegate <FWeakObjectPtr> DamageTaken;
+
+};
